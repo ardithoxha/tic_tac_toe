47209db007ea6c88370b7c1434134bca1a0a01d9
Ardit <ardit.hoxha@gmail.com>
2021-11-13 10:37:28 -0500

    basic UI design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Square(props) {
+  return (
+    <div className="square"></div>
+  );
+}
+
+function Board() {
+  return (
+    <div className="board">
+      <Square box={0}/>
+      <Square box={1}/>
+      <Square box={2}/>
+      <Square box={3}/>
+      <Square box={4}/>
+      <Square box={5}/>
+      <Square box={6}/>
+      <Square box={7}/>
+      <Square box={8}/>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Next Player is:</p>
+      <Board />
     </div>
   );
 }
